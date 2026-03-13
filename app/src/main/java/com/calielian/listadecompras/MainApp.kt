b1905fae31499e3f522bb397f6282a7187a9d8a2
calielian <calielian@proton.me>
2026-03-12 21:17:28 -0300

    Initial commit

---
+++ app/src/main/java/com/calielian/listadecompras/MainApp.kt
@@ -0,0 +1,12 @@
+package com.calielian.listadecompras
+
+import android.app.Application
+import com.google.android.material.color.DynamicColors
+
+class MainApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        DynamicColors.applyToActivitiesIfAvailable(this)
+    }
+}
